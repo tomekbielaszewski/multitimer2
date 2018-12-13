8008a064985da599c11afca9d41b616b4f74f429
Grizwold <tomekbielaszewski@gmail.com>
2018-12-13 22:51:28 +0100

    Application window class placeholder

---
+++ src/main/java/pl/grizwold/multitimer/gui/frames/ApplicationWindow.java
@@ -0,0 +1,12 @@
+package pl.grizwold.multitimer.gui.frames;
+
+import pl.grizwold.multitimer.gui.model.GuiEvent;
+
+import java.util.Queue;
+
+public class ApplicationWindow {
+
+
+    public ApplicationWindow(Queue<GuiEvent> guiCommunicationQueue) {
+    }
+}
