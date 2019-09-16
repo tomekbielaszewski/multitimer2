7c6f123c80672be1b5e35f6c1855ff36926c0fb5
Grizwold <tomekbielaszewski@gmail.com>
2019-09-16 23:25:37 +0200

    dummy dao implementation

--- src/main/java/pl/grizwold/multitimer/logic/PauseDao.java
+++ src/main/java/pl/grizwold/multitimer/logic/PauseDao.java
@@ -3,19 +3,26 @@ package pl.grizwold.multitimer.logic;
 import org.springframework.stereotype.Service;
 import pl.grizwold.multitimer.logic.model.Pause;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class PauseDao {
-    public void save(Pause pause) {
+    private List<Pause> pauses = new ArrayList<>();
 
+    public void save(Pause pause) {
+        this.pauses.add(pause);
     }
 
     public Pause get(UUID id) {
-        return null;
+        return pauses.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
     }
 
     public void remove(UUID id) {
-
+        pauses.remove(get(id));
     }
 }
--- src/main/java/pl/grizwold/multitimer/logic/TimerDao.java
+++ src/main/java/pl/grizwold/multitimer/logic/TimerDao.java
@@ -1,34 +1,48 @@
 package pl.grizwold.multitimer.logic;
 
 import org.springframework.stereotype.Service;
+import pl.grizwold.multitimer.logic.exception.TimerNotFoundException;
 import pl.grizwold.multitimer.logic.model.Timer;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class TimerDao {
+    private List<Timer> timers = new ArrayList<>();
+
     List<Timer> getExpired() {
-        return null;
+        LocalDateTime now = LocalDateTime.now();
+        return timers.stream()
+                .filter(t -> now.isAfter(t.getFinish()))
+                .filter(t -> !t.isFinished())
+                .collect(Collectors.toList());
     }
 
     UUID save(Timer timer) {
-        return null;
+        timers.add(timer);
+        return timer.getId();
     }
 
     void markAsFinished(UUID id) {
-
+        get(id).setFinished(true);
     }
 
     void stop(UUID id) {
+        get(id).setFinish(LocalDateTime.now());
+    }
 
+    public void markAsPaused(boolean paused, UUID id) {
+        get(id).setPaused(paused);
     }
 
     public Timer get(UUID id) {
-        return null;
-    }
-
-    public void markAsPaused(boolean b, UUID id) {
-
+        return timers.stream()
+                .filter(t -> t.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new TimerNotFoundException(id));
     }
 }
