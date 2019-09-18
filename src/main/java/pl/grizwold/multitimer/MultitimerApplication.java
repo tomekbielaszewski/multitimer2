3288ce19b1c7b93b6c9fcbfa683fe37f7f5b16cd
tbielaszewski <tomekbielaszewski@gmail.com>
2019-09-18 22:29:19 +0200

    enabled spring scheduling, temporally removed jpa

--- src/main/java/pl/grizwold/multitimer/MultitimerApplication.java
+++ src/main/java/pl/grizwold/multitimer/MultitimerApplication.java
@@ -2,7 +2,9 @@ package pl.grizwold.multitimer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class MultitimerApplication {
     public static void main(String[] args) {
